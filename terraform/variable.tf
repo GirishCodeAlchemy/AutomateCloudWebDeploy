013c8a39f3a07d25103e6398a652ee9efbda5e7d
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-02-05 23:15:11 -0500

    Added the kube configuration for the kubernetes provider

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,19 @@
-variable "KUBECONFIG" {
-  type = string
+variable "KUBE_HOST" {
+  type        = string
+  description = "Specifies the URL of the Kubernetes API server."
+}
+
+variable "KUBE_CLUSTER_CA_CERT_DATA" {
+  type        = string
+  description = "The CA certificate used to verify the authenticity of the Kubernetes cluster"
+}
+
+variable "KUBE_CTX_AUTH_INFO" {
+  type        = string
+  description = "Describes the authentication context."
+}
+
+variable "KUBE_TOKEN" {
+  type        = string
+  description = "Represents the authentication token"
 }
